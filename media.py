eabb7ef2dd6e69704a0a7b17176b5e47ade7bebb
Michal Ondrasek <michal.ondrasek@gmail.com>
2017-08-16 10:42:50 +0200

    [MASTER] media.py file updated with proper comments

--- media.py
+++ media.py
@@ -1,9 +1,12 @@
-import webbrowser
+import webbrowser  # importing the webbrowser library
+
+
+'''Movie class to specify instance variables and methods for movies'''
 
 
 class Movie():
-    """ This class provides a way to store movie related information """
 
+    """ Constructor method for movie instances, creates instance variables """
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         self.title = movie_title
@@ -11,5 +14,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    '''instance method for youtube movie trailers'''
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
